2d3654ebce23da4fc90feac882733b99e878660f
myu-238 <118711569+myu-238@users.noreply.github.com>
2023-08-13 22:49:41 -0400

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -22,40 +22,66 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+}
+
+function displayForecast(response) {
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
 
-    let days = ["Thu", "Fri", "Sat", "Sun"];
-
     let forecastHTML = `<div class="row">`;
-    days.forEach(function (day) {
-        forecastHTML =
-            forecastHTML +
-            `
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
+            forecastHTML =
+                forecastHTML +
+                `
                             <div class="col-2">
-                                <div class="weather-forecast-date">${day}</div>
+                                <div class="weather-forecast-date">${formatDay(
+                                    forecastDay.time
+                                )}</div>
                                 <img
-                                    src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                                    src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                                        forecastDay.condition.icon
+                                    }.png"
                                     alt=""
                                     width="42"
                                 />
                                 <div class="weather-forecast-temperatures">
                                     <span
                                         class="weather-forecast-temperature-max"
-                                        >18°</span
+                                        >${Math.round(
+                                            forecastDay.temperature.maximum
+                                        )}°</span
                                     >
                                     <span
                                         class="weather-forecast-temperature-min"
                                     >
-                                        12°</span
+                                        ${Math.round(
+                                            forecastDay.temperature.minimum
+                                        )}°</span
                                     >
                                 </div>
                             </div>
          `;
+        }
     });
 
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "28842fobf1a190b0t62a268683055905";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -81,6 +107,8 @@ function displayTemperature(response) {
         `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
     );
     iconElement.setAttribute("alt", response.data.condition.description);
+
+    getForecast(response.data.coordinates);
 }
 
 function search(city) {
@@ -115,8 +143,6 @@ function displayCelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
-
-displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
